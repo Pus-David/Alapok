2459281032ea411bda748aeb686fc3abc53c7f1b
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-10 10:34:52 +0200

    1. feladat

--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Auto;
+Autok piros = new Autok();
+piros.Marka = "Toyota";
+piros.Eletkor = 5;
+
+Autok kek = new Autok();
+Console.WriteLine("Kérem az autó márkáját:");
+kek.Marka = Console.ReadLine();
+Console.WriteLine("kéream az auto eletkorat");
+kek.Eletkor = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"1. példány: {piros.Marka},{piros.Eletkor}");
+Console.WriteLine($"1. példány: {kek.Marka},{kek.Eletkor}");
