ac8db2d8d43a19da44787ba7dc798fc7158669de
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-10 11:44:58 +0200

    feladat 4

---
+++ Jatekos/Jatek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jatekos
+{
+    internal class Jatek
+    {
+        private int elet;
+        public string Nev { get; set;  }
+        public int Elet 
+        {
+            get { return elet; } 
+            set 
+            {
+                if (value < 0) elet = 0;
+                else if (value > 100) elet = 100;
+                else elet = value;
+            } 
+        }
+    }
+}
---
+++ Jatekos/Program.cs
@@ -0,0 +1,9 @@
+﻿using Jatekos;
+
+Jatek jatekos = new Jatek();
+
+jatekos.Nev = "Jatékos1";
+jatekos.Elet = 120; //Elet will be set to 100 due to the setter logic
+Console.WriteLine($"Név: {jatekos.Nev}, Élet: {jatekos.Elet}");
+jatekos.Elet = -20;
+Console.WriteLine($"Név: {jatekos.Nev}, Élet: {jatekos.Elet}");
